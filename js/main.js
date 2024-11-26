17f3ebedbac924830f4e37011484b8e5aa0d9a5d
Sarvarbek <sarvarbekodilxojayev898@gmail.com>
2024-11-26 19:02:56 +0500

    start

---
+++ js/main.js
@@ -0,0 +1,267 @@
+// // 1-misol
+// var  yosh = Number(prompt("Yoshingizni kiriting: "))
+// if(yosh > 50){
+//     console.log("Siz kattasiz");
+// }else{
+//     console.log("Siz yoshsiz");
+// }
+
+// // 2-misol
+// var narx=Number(prompt(""))
+// if( narx>3000){
+//     console.log("Qimmat");
+// }else{
+//     console.log("arzon");
+// }
+
+// // 3-misol
+// var ball = Number(prompt("Ballingizni kiriting:"))
+// if (ball < 60) {
+//     console.log("Balingiz past");
+// }
+//  else if (ball > 100 && ball < 130) {
+//     console.log("Natijangiz yaxshi");
+// } else if(ball > 130){
+//     console.log("Natijangiz zo'r");
+// }
+//  else {
+//     console.log("Siz xato ma'lumot kiritdingiz");
+// }
+
+// // 4-misol
+// var codial=prompt("Parolni kiriting")
+// if(codial =="codial"){
+//     console.log("salom")
+// }else{
+//     console.log("xato")
+// }
+
+// // 5-misol
+// var maosh = Number(prompt("Maoshingiz qancha"))
+// if (maosh > "1500" && maosh < "2000"){
+//     console.log("Kasbingiz nima");
+// } else if(maosh > "500" && maosh < "700"){
+//     console.log("kasbingizni o'zgartiring");
+// }
+// else{
+//     console.log( " Ishingizni  o'zgartiring");
+// }
+
+// // 6-misol
+// var Ali=prompt("Do'stingizni ismi")
+// if(Ali =="Ali"){
+//     console.log("Salom");
+// }else{
+//     console.log("Ko’rinmayapsan dostim, nima qilyapsan?");
+// }
+
+// // 7-misol
+// var ovqat = prompt("Qanday ovqat buyurasiz")
+// if (ovqat == "somsa") {
+//     console.log("3000");
+// }
+// else if (ovqat == "palov") {
+//     console.log("15000");
+// } else if (ovqat == "gumma") {
+//     console.log("2000");
+// }
+// else {
+//     console.log("Bizda unday ovqat yoq");
+// }
+
+// // 8-misol
+
+
+
+
+
+// // 9-misol
+// var yosh = +prompt("Yoshingizni kiriting")
+// if (yosh >= 18) {
+//     console.log("Yosh katta")
+// }
+// else if (yosh < 18) {
+//     console.log("Yosh kichik")
+// }
+
+// // 10-misol
+// const son1 = Number(prompt("Birinchi son"))
+// const son2 = Number(prompt("Ikkinchi son"))
+// if (son1 == "son2" ){
+//     console.log("Sonlar teng");
+// }else{
+//     console.log("Son teng emas");
+// }
+
+
+// // 11-misol
+// const qavat=Number(prompt("Nechinchi qavatda turasiz"))
+//    if(qavat>=4 && qavat<=10){
+//     console.log("Liftdan foydalaning chiqishingiz mumkin");
+//    }else if (qavat<4) {
+//     console.log("Zinadan chiqishingiz mumkin");
+//    }
+
+
+
+
+
+// QUDRAT ABDURAHIMOV
+
+
+
+
+
+
+// // 1-misol
+// const son = Number(prompt("Son kiriting"))
+// if (son >= 0 ) {
+//    if(son + 1);{
+//     console.log(son+1)
+//    }
+// }else{
+//     console.log(son);
+//    }
+
+// // 2-misol
+// const son = Number(prompt("Son kiriting"))
+// if (son > 0) {
+//     if (son + 1); {
+//         console.log(son + 1)
+//     }
+// } else {
+//     console.log(son - 2);
+// }
+
+// // 3-misol
+// const son = Number(prompt("Son kiriting"))
+// if (son > 0) {
+//     if (son + 1); {
+//         console.log(son + 1);
+//     }
+// }else if (son == 0) {
+//     console.log(son + 10);
+// }
+//  else {
+//     console.log(son - 2);
+// }
+
+// // 4-misol
+// const son1 = Number(prompt("Birinchi sonni kiriting"))
+// const son2 = Number(prompt("Ikkinchi sonni kiriting"))
+// const son3 = Number(prompt("Uchinchi sonni kiriting"))
+// if (son1 > 0) {
+//     console.log(son1)
+// }
+// if (son2 > 0) {
+//     console.log(son2)
+// }
+// if (son3 > 0) {
+//     console.log(son3)
+// }
+
+// // 5-misol
+// const son1 = Number(prompt("Birinchi sonni kiriting"))
+// const son2 = Number(prompt("Ikkinchi sonni kiriting"))
+// const son3 = Number(prompt("Uchinchi sonni kiriting"))
+// if (son1 > 0) {
+//     console.log(son1)
+// }else(
+//     console.log("Birinchi son musbat emas")
+// )
+// if (son2 > 0) {
+//     console.log(son2)
+// }else(
+//     console.log("Ikkinchi son musbat emas")
+// )
+// if (son3 > 0) {
+//     console.log(son3)
+// }else(
+//         console.log("Uchinchi son musbat emas")
+//     )
+
+// // 6-misol
+// const son1 = Number(prompt("Birinchi sonni kiriting"))
+// const son2 = Number(prompt("Ikkinchi sonni kiriting"))
+// if (son1 > son2) {
+//     console.log("Birinchi son kotta", son1);
+// } else (
+//     console.log( "Ikkinchi son kotta", son2)
+// )
+
+// 7-misol
+
+
+
+// // 8-misol
+// const son1 = Number(prompt("Birinchi sonni kiriting"))
+// const son2 = Number(prompt("Ikkinchi sonni kiriting"))
+// if (son1 > son2) {
+//     console.log(son1, son2);
+// } else (
+//     console.log(son2, son1)
+// )
+
+// // 9-misol
+// const son1 = Number(prompt("Birinchi sonni kiriting"))
+// const son2 = Number(prompt("Ikkinchi sonni kiriting"))
+// if (son1 > son2) {
+//     console.log(son2, son1);
+// } else (
+//     console.log(son1, son2)
+// )
+
+// // 10-misol
+// const son1 = Number(prompt("Birinchi sonni kiriting"))
+// const son2 = Number(prompt("Ikkinchi sonni kiriting"))
+// if (son1 != son2) {
+//     console.log(son1+son2);
+// } else (
+//     console.log(0)
+// )
+
+// // 11-misol
+// const son1 = Number(prompt("Birinchi sonni kiriting"))
+// const son2 = Number(prompt("Ikkinchi sonni kiriting"))
+// if (son1 > son2) {
+//     console.log(son1);
+// } else if  (son1 < son2)  {
+//     console.log(son2)
+// }else if ( son1 == son2){
+//     console.log(0)
+// }
+ 
+// // 12-misol
+// const son1 = Number(prompt("Birinchi sonni kiriting"))
+// const son2 = Number(prompt("Ikkinchi sonni kiriting"))
+// const son3 = Number(prompt("Uchinchi sonni kiriting"))
+// if( son1<=son2 && son1<=son3){
+//     console.log(son1)
+// }else if (son2<=son1 && son2<=son3){
+//     console.log(son2)
+// }else(
+//     console.log(son3)
+// )
+
+// // 13-misol
+// const a = Number(prompt("Birinchi sonni kiriting"))
+// const b = Number(prompt("Ikkinchi sonni kiriting"))
+// const c = Number(prompt("Uchinchi sonni kiriting"))
+// if ((a > b && a < c) || (a > c && a < b)) {
+//     console.log(a)
+// } else if ((b > a && b < c) || (b > c && b < a)) {
+//     console.log(b)
+// } else {
+//     console.log(c)
+// }
+
+// 14-misol
+
+
+
+
+
+
+
+
+
